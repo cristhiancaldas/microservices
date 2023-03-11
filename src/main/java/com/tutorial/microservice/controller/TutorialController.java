783f5f2b1a2c9aa48e6a9bcc206bc67e388ce4af
“cristhian <c.caldas.m@gmail.com>
2023-03-11 10:11:17 -0500

    subiendo cambios en el proyecto

--- src/main/java/com/tutorial/microservice/controller/TutorialController.java
+++ src/main/java/com/tutorial/microservice/controller/TutorialController.java
@@ -2,6 +2,11 @@ package com.tutorial.microservice.controller;
 
 import com.tutorial.microservice.entity.Tutorial;
 import com.tutorial.microservice.service.TutorialService;
+import io.swagger.v3.oas.annotations.Operation;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,31 +18,41 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/tutorial/v1")
 public class TutorialController {
-
+    private static Logger logger = LoggerFactory.getLogger(TutorialController.class);
     @Autowired
     TutorialService tutorialService;
 
+    @Operation(summary = "Add Tutorial")
     @PostMapping
     public ResponseEntity<Tutorial> addTutorial(@RequestBody Tutorial tutorial){
+        logger.info("Init Controller addTutorial");
         Tutorial addTutorial = tutorialService.addTutorial(tutorial);
+        logger.info("Fin Controller addTutorial");
+
         return ResponseEntity.ok(addTutorial);
     }
 
+    @Operation(summary = "Get all Tutorial")
     @GetMapping
     public ResponseEntity<List<Tutorial>> getAllTutorial(){
+        logger.info("Init Controller getAllTutorial");
          List<Tutorial> lstTutorial=  tutorialService.getAllTutorial();
          return    ResponseEntity.ok(lstTutorial);
     }
 
+    @Operation(summary = "Get Tutorial By ID")
     @GetMapping("/{idTutorial}")
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable String idTutorial){
+        logger.info("Init Controller getTutorialById");
         Optional<Tutorial> tutorial=  tutorialService.getTutorialById(idTutorial);
         return new ResponseEntity<>(tutorial.get(), HttpStatus.OK);
     }
 
 
+    @Operation(summary = "Delete Tutorial By ID")
     @DeleteMapping("/{idTutorial}")
     public ResponseEntity<HttpStatus> deleteTutorial(@PathVariable String idTutorial){
+        logger.info("Init Controller deleteTutorial");
         tutorialService.deleteTutorial(idTutorial);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- src/main/java/com/tutorial/microservice/service/TutorialService.java
+++ src/main/java/com/tutorial/microservice/service/TutorialService.java
@@ -1,7 +1,11 @@
 package com.tutorial.microservice.service;
 
+
 import com.tutorial.microservice.entity.Tutorial;
 import com.tutorial.microservice.repository.TutorialRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,24 +16,31 @@ import java.util.UUID;
 @Service
 public class TutorialService {
 
+    private static Logger logger = LoggerFactory.getLogger(TutorialService.class);
+
     @Autowired
     TutorialRepository tutorialRepository;
 
-    public Tutorial addTutorial (Tutorial tutorial){
+    public Tutorial addTutorial(Tutorial tutorial) {
+        logger.info("Init Service addTutorial");
         tutorial.setIdTutorial(UUID.randomUUID().toString());
+        logger.info("Fin Service addTutorial");
+
         return tutorialRepository.save(tutorial);
     }
 
-    public List<Tutorial> getAllTutorial(){
-      return  tutorialRepository.findAll();
+    public List<Tutorial> getAllTutorial() {
+        logger.info("Init Service getAllTutorial");
+        return tutorialRepository.findAll();
     }
 
-    public Optional<Tutorial> getTutorialById(String idTutorial){
+    public Optional<Tutorial> getTutorialById(String idTutorial) {
+        logger.info("Init Service getTutorialById");
         return tutorialRepository.findById(idTutorial);
     }
 
-    public void deleteTutorial(String idTutorial){
+    public void deleteTutorial(String idTutorial) {
+        logger.info("Init Service deleteTutorial");
         tutorialRepository.deleteById(idTutorial);
     }
-
 }
